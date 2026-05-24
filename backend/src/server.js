c70992c44e7c27583d72f4b795311dcf06609ca1
asya-xo <gulsumerd38@gmail.com>
2026-05-24 21:23:05 +0200

    chore: add request body size limit

--- backend/src/server.js
+++ backend/src/server.js
@@ -16,7 +16,7 @@ app.use(cors({
   origin: ['http://localhost:5173', 'http://localhost:5174'],
   credentials: true
 }));
-app.use(express.json());
+app.use(express.json({ limit: '10kb' }));
 app.use(cookieParser());
 
 
